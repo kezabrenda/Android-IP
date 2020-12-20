b845a96f8ae5260daad8d3392bd661420932ca35
keza1brenda <keza1brenda@gmail.com>
2020-12-20 19:37:49 +0200

    add functionality to first button

--- app/src/main/java/com/example/myindependentproject/MainActivity.java
+++ app/src/main/java/com/example/myindependentproject/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myindependentproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindPlacesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindPlacesButton = (Button)findViewById(R.id.findPlacesButton);
+        mFindPlacesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LocateMeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
