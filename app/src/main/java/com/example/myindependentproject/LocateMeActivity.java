717a8c16a604715fe1969e083388c88704f23dbc
keza1brenda <keza1brenda@gmail.com>
2020-12-21 04:23:24 +0200

    Add more functionality to class

--- app/src/main/java/com/example/myindependentproject/LocateMeActivity.java
+++ app/src/main/java/com/example/myindependentproject/LocateMeActivity.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class LocateMeActivity extends AppCompatActivity {
-    @BindView(R.id.findPlacesButton) private Button mCleaningPlacesButton;
-    @BindView(R.id.findPlacesButton) private Button mMobileAgentButton;
+public class LocateMeActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.cleaningPlacesButton) private Button mCleaningPlacesButton;
+    @BindView(R.id.mobileAgentButton) private Button mMobileAgentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locate_me);
-        Intent intent = getIntent();
-        String location = intent.getStringExtra("location");
+        ButterKnife.bind(this);
+
+        mCleaningPlacesButton.setOnClickListener(this);
+        mMobileAgentButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mCleaningPlacesButton) {
+            Intent intent = new Intent(LocateMeActivity.this, CleaningActivity.class);
+            String location = intent.getStringExtra("location");
+            startActivity(intent);
+        }
     }
 }
